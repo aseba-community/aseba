d5982455ef0901c4bbfd4dc57b971422737bea48
Valentin Longchamp <valentin.longchamp@gmail.com>
2010-03-04 16:02:42 +0000

    config for medulla dbus, and added --system option for test-medulla

--- medulla/test-medulla.py
+++ medulla/test-medulla.py
@@ -4,6 +4,7 @@
 import dbus
 import dbus.mainloop.glib
 import gobject
+from optparse import OptionParser
 
 def get_variables_reply(r):
 	print 'variables:'
@@ -15,16 +16,28 @@ def get_variables_error(e):
 	loop.quit()
 
 if __name__ == '__main__':
+	parser = OptionParser()
+	parser.add_option("-s", "--system", action="store_true", dest="system", default=False,
+			help="use the system bus instead of the session bus")
+
+	(options, args) = parser.parse_args()
+
 	dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 	
-	bus = dbus.SessionBus()
+	if options.system:
+		bus = dbus.SystemBus()
+	else:
+		bus = dbus.SessionBus()
+	
 	network = dbus.Interface(bus.get_object('ch.epfl.mobots.Aseba', '/'), dbus_interface='ch.epfl.mobots.AsebaNetwork')
 	print network.GetNodesList()
 
 	#network.SetVariable("e-puck 1", "leftSpeed", [10])
 	#network.SetVariable("e-puck 1", "rightSpeed", [-10])
 
-	print network.GetVariable("e-puck 1", "leftSpeed",reply_handler=get_variables_reply,error_handler=get_variables_error)
+	network.SendEvent(0, [])
+
+	#print network.GetVariable("e-puck 1", "leftSpeed",reply_handler=get_variables_reply,error_handler=get_variables_error)
 
 	print 'starting loop'
 	loop = gobject.MainLoop()
