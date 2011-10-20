aseba-community/aseba

commit f9d4008044719eb2726843cb96e2f5b75708c6ec

Fixed a crash in the parser.
The bug has been introduced by commit 55f9b1e73cb7b23f08d60a9308592c5ad81df7df,
and futher amplified by 75394b2736a212acdadd08280c422b54614fe4b1. Access to arrays
was not properly managed, resulting in invalid type cast.

compiler/parser.cpp
@@ -360,6 +360,10 @@ namespace Aseba
 	{
 		expect(Token::TOKEN_STRING_LITERAL);
 		
+		const Token::Type compoundAssignment[] = { Token::TOKEN_OP_ADD_EQUAL, Token::TOKEN_OP_NEG_EQUAL,
+							   Token::TOKEN_OP_MULT_EQUAL, Token::TOKEN_OP_DIV_EQUAL, Token::TOKEN_OP_MOD_EQUAL,
+							   Token::TOKEN_OP_BIT_OR_EQUAL, Token::TOKEN_OP_BIT_XOR_EQUAL, Token::TOKEN_OP_BIT_NOT_EQUAL,
+							   Token::TOKEN_OP_SHIFT_LEFT_EQUAL, Token::TOKEN_OP_SHIFT_RIGHT_EQUAL};
 		std::wstring varName = tokens.front().sValue;
 		SourcePos varPos = tokens.front().pos;
 		VariablesMap::const_iterator varIt(findVariable(varName, varPos));
@@ -402,8 +406,10 @@ namespace Aseba
 		}
 		else if ((tokens.front() == Token::TOKEN_OP_PLUS_PLUS) || (tokens.front() == Token::TOKEN_OP_MINUS_MINUS))
 			assignment->children.push_back(parseIncrementAssignment(l_value));
-		else
+		else if (IS_ONE_OF(compoundAssignment))
 			assignment->children.push_back(parseCompoundAssignment(l_value));
+		else
+			throw Error(varPos, WFormatableString(L"Expecting assignement, found %0 instead").arg(varName));
 		
 		return assignment.release();
 	}
@@ -417,8 +423,20 @@ namespace Aseba
 		SourcePos pos = tokens.front().pos;
 		tokens.pop_front();
 
-		// convert the l_value from a StoreNode to a LoadNode
-		std::auto_ptr<LoadNode> load(new LoadNode( dynamic_cast<StoreNode*>(l_value)));
+		// convert the l_value from a StoreNode / ArrayWriteNode to a LoadNode / ArrayReadNode respectively
+		std::auto_ptr<Node> load;
+		StoreNode* store = dynamic_cast<StoreNode*>(l_value);
+		ArrayWriteNode* read = dynamic_cast<ArrayWriteNode*>(l_value);
+		if (store)
+			// scalar
+			load.reset(new LoadNode(store));
+		else if (read)
+			// array
+			load.reset(new ArrayReadNode(read));
+		else
+			// oops
+			throw Error(pos, WFormatableString(L"Unexpected error: missing scalar or array assignment"));
+
 		// get the r_value
 		std::auto_ptr<Node> node(parseBinaryOrExpression());
 

compiler/tree.h
@@ -53,6 +53,7 @@ namespace Aseba
 		
 		//! Constructor
 		Node(const SourcePos& sourcePos) : sourcePos(sourcePos) { }
+		virtual Node* clone() const = 0;
 		
 		virtual ~Node();
 		
@@ -88,6 +89,7 @@ namespace Aseba
 	{
 		//! Constructor
 		BlockNode(const SourcePos& sourcePos) : Node(sourcePos) { }
+		virtual BlockNode* clone() const {return(new BlockNode(*this));}
 		
 		virtual Node* optimize(std::wostream* dump);
 		virtual void emit(PreLinkBytecode& bytecodes) const;
@@ -100,7 +102,8 @@ namespace Aseba
 	{
 		//! Constructor
 		ProgramNode(const SourcePos& sourcePos) : BlockNode(sourcePos) { }
-		
+		virtual ProgramNode* clone() const {return(new ProgramNode(*this));}
+
 		virtual void emit(PreLinkBytecode& bytecodes) const;
 		virtual std::wstring toWString() const { return L"ProgramBlock"; }
 		virtual std::wstring toNodeName() const { return L"program block"; }
@@ -113,7 +116,8 @@ namespace Aseba
 	{
 		//! Constructor
 		AssignmentNode(const SourcePos& sourcePos) : Node(sourcePos) { }
-		
+		virtual AssignmentNode* clone() const {return(new AssignmentNode(*this));}
+
 		virtual ReturnType typeCheck() const;
 		virtual Node* optimize(std::wostream* dump);
 		virtual void emit(PreLinkBytecode& bytecodes) const;
@@ -132,7 +136,8 @@ namespace Aseba
 		
 		//! Constructor
 		IfWhenNode(const SourcePos& sourcePos) : Node(sourcePos) { }
-		
+		virtual IfWhenNode* clone() const {return(new IfWhenNode(*this));}
+
 		virtual ReturnType typeCheck() const;
 		virtual Node* optimize(std::wostream* dump);
 		virtual void emit(PreLinkBytecode& bytecodes) const;
@@ -153,7 +158,8 @@ namespace Aseba
 		
 		//! Constructor
 		FoldedIfWhenNode(const SourcePos& sourcePos) : Node(sourcePos) { }
-		
+		virtual FoldedIfWhenNode* clone() const {return(new FoldedIfWhenNode(*this));}
+
 		virtual Node* optimize(std::wostream* dump);
 		virtual unsigned getStackDepth() const;
 		virtual void emit(PreLinkBytecode& bytecodes) const;
@@ -168,7 +174,8 @@ namespace Aseba
 	{
 		//! Constructor
 		WhileNode(const SourcePos& sourcePos) : Node(sourcePos) { }
-		
+		virtual WhileNode* clone() const {return(new WhileNode(*this));}
+
 		virtual ReturnType typeCheck() const;
 		virtual Node* optimize(std::wostream* dump);
 		virtual void emit(PreLinkBytecode& bytecodes) const;
@@ -186,7 +193,8 @@ namespace Aseba
 		
 		//! Constructor
 		FoldedWhileNode(const SourcePos& sourcePos) : Node(sourcePos) { }
-		
+		virtual FoldedWhileNode* clone() const {return(new FoldedWhileNode(*this));}
+
 		virtual Node* optimize(std::wostream* dump);
 		virtual unsigned getStackDepth() const;
 		virtual void emit(PreLinkBytecode& bytecodes) const;
@@ -201,7 +209,8 @@ namespace Aseba
 		unsigned eventId; //!< the event id associated with this context
 		
 		EventDeclNode(const SourcePos& sourcePos, unsigned eventId = 0);
-		
+		virtual EventDeclNode* clone() const {return(new EventDeclNode(*this));}
+
 		virtual ReturnType typeCheck() const { return TYPE_UNIT; }
 		virtual Node* optimize(std::wostream* dump);
 		virtual void emit(PreLinkBytecode& bytecodes) const;
@@ -219,7 +228,8 @@ namespace Aseba
 		
 		//! Constructor
 		EmitNode(const SourcePos& sourcePos) : Node(sourcePos) { }
-		
+		virtual EmitNode* clone() const {return(new EmitNode(*this));}
+
 		virtual ReturnType typeCheck() const { return TYPE_UNIT; }
 		virtual Node* optimize(std::wostream* dump);
 		virtual void emit(PreLinkBytecode& bytecodes) const;
@@ -234,7 +244,8 @@ namespace Aseba
 		unsigned subroutineId; //!< the associated subroutine
 		
 		SubDeclNode(const SourcePos& sourcePos, unsigned subroutineId);
-		
+		virtual SubDeclNode* clone() const {return(new SubDeclNode(*this));}
+
 		virtual ReturnType typeCheck() const { return TYPE_UNIT; }
 		virtual Node* optimize(std::wostream* dump);
 		virtual void emit(PreLinkBytecode& bytecodes) const;
@@ -249,7 +260,8 @@ namespace Aseba
 		unsigned subroutineId; //!< the subroutine to call
 		
 		CallSubNode(const SourcePos& sourcePos, unsigned subroutineId);
-		
+		virtual CallSubNode* clone() const {return(new CallSubNode(*this));}
+
 		virtual Node* optimize(std::wostream* dump);
 		virtual void emit(PreLinkBytecode& bytecodes) const;
 		virtual std::wstring toWString() const;
@@ -265,7 +277,8 @@ namespace Aseba
 		
 		BinaryArithmeticNode(const SourcePos& sourcePos) : Node(sourcePos) { }
 		BinaryArithmeticNode(const SourcePos& sourcePos, AsebaBinaryOperator op, Node *left, Node *right);
-		
+		virtual BinaryArithmeticNode* clone() const {return(new BinaryArithmeticNode(*this));}
+
 		void deMorganNotRemoval();
 		
 		virtual ReturnType typeCheck() const;
@@ -291,7 +304,8 @@ namespace Aseba
 		//! Constructor
 		UnaryArithmeticNode(const SourcePos& sourcePos) : Node(sourcePos) { }
 		UnaryArithmeticNode(const SourcePos& sourcePos, AsebaUnaryOperator op, Node *child);
-		
+		virtual UnaryArithmeticNode* clone() const {return(new UnaryArithmeticNode(*this));}
+
 		virtual ReturnType typeCheck() const;
 		virtual Node* optimize(std::wostream* dump);
 		virtual void emit(PreLinkBytecode& bytecodes) const;
@@ -309,7 +323,8 @@ namespace Aseba
 		
 		//! Constructor
 		ImmediateNode(const SourcePos& sourcePos, int value) : Node(sourcePos), value(value) { }
-		
+		virtual ImmediateNode* clone() const {return(new ImmediateNode(*this));}
+
 		virtual ReturnType typeCheck() const { return TYPE_INT; }
 		virtual Node* optimize(std::wostream* dump);
 		virtual unsigned getStackDepth() const;
@@ -326,7 +341,8 @@ namespace Aseba
 		
 		//! Constructor
 		StoreNode(const SourcePos& sourcePos, unsigned varAddr) : Node(sourcePos), varAddr(varAddr) { }
-		
+		virtual StoreNode* clone() const {return(new StoreNode(*this));}
+
 		virtual ReturnType typeCheck() const { return TYPE_UNIT; }
 		virtual Node* optimize(std::wostream* dump);
 		virtual void emit(PreLinkBytecode& bytecodes) const;
@@ -343,6 +359,7 @@ namespace Aseba
 		//! Constructor
 		LoadNode(const SourcePos& sourcePos, unsigned varAddr) : Node(sourcePos), varAddr(varAddr) { }
 		LoadNode(const StoreNode* store) : Node(store->sourcePos), varAddr(store->varAddr) {}
+		virtual LoadNode* clone() const {return(new LoadNode(*this));}
 
 		virtual ReturnType typeCheck() const { return TYPE_INT; }
 		virtual Node* optimize(std::wostream* dump);
@@ -352,23 +369,6 @@ namespace Aseba
 		virtual std::wstring toNodeName() const { return L"variable access (read)"; }
 	};
 
-	//! Node for reading from an array.
-	//! children[0] is the index in the array
-	struct ArrayReadNode : Node
-	{
-		unsigned arrayAddr; //!< address of the first element of the array
-		unsigned arraySize; //!< size of the array, might be used to assert compile-time access checks
-		std::wstring arrayName; //!< name of the array (for debug)
-		
-		ArrayReadNode(const SourcePos& sourcePos, unsigned arrayAddr, unsigned arraySize, const std::wstring &arrayName);
-		
-		virtual ReturnType typeCheck() const { return TYPE_INT; }
-		virtual Node* optimize(std::wostream* dump);
-		virtual void emit(PreLinkBytecode& bytecodes) const;
-		virtual std::wstring toWString() const;
-		virtual std::wstring toNodeName() const { return L"array access (read)"; }
-	};
-	
 	//! Node for writing to an array. Value to store is supposed to be on the stack already
 	//! children[0] is the index in the array
 	struct ArrayWriteNode : Node
@@ -378,7 +378,8 @@ namespace Aseba
 		std::wstring arrayName; //!< name of the array (for debug)
 		
 		ArrayWriteNode(const SourcePos& sourcePos, unsigned arrayAddr, unsigned arraySize, const std::wstring &arrayName);
-		
+		virtual ArrayWriteNode* clone() const {return(new ArrayWriteNode(*this));}
+
 		virtual ReturnType typeCheck() const { return TYPE_UNIT; }
 		virtual Node* optimize(std::wostream* dump);
 		virtual void emit(PreLinkBytecode& bytecodes) const;
@@ -386,6 +387,29 @@ namespace Aseba
 		virtual std::wstring toNodeName() const { return L"array access (write)"; }
 	};
 	
+	//! Node for reading from an array.
+	//! children[0] is the index in the array
+	struct ArrayReadNode : Node
+	{
+		unsigned arrayAddr; //!< address of the first element of the array
+		unsigned arraySize; //!< size of the array, might be used to assert compile-time access checks
+		std::wstring arrayName; //!< name of the array (for debug)
+
+		ArrayReadNode(const SourcePos& sourcePos, unsigned arrayAddr, unsigned arraySize, const std::wstring &arrayName);
+		ArrayReadNode(const ArrayWriteNode* write) :
+			Node(write->sourcePos), arrayAddr(write->arrayAddr), arraySize(write->arraySize), arrayName(write->arrayName)
+		{
+			children.push_back((write->children[0])->clone());
+		}
+		virtual ArrayReadNode* clone() const {return(new ArrayReadNode(*this));}
+
+		virtual ReturnType typeCheck() const { return TYPE_INT; }
+		virtual Node* optimize(std::wostream* dump);
+		virtual void emit(PreLinkBytecode& bytecodes) const;
+		virtual std::wstring toWString() const;
+		virtual std::wstring toNodeName() const { return L"array access (read)"; }
+	};
+
 	//! Node for calling a native function
 	//! may have children for pushing constants somewhere
 	struct CallNode : Node
@@ -394,7 +418,8 @@ namespace Aseba
 		std::vector<unsigned> argumentsAddr; //!< address of all arguments
 		
 		CallNode(const SourcePos& sourcePos, unsigned funcId);
-		
+		virtual CallNode* clone() const {return(new CallNode(*this));}
+
 		virtual ReturnType typeCheck() const { return TYPE_UNIT; }
 		virtual Node* optimize(std::wostream* dump);
 		virtual unsigned getStackDepth() const;

commit 6bcfe42df3715a3f5a7d10035f1c7b0bbc41f779

Fixed regression.
&= operator has been broken by commit f9d4008044719eb2726843cb96e2f5b75708c6ec.

compiler/parser.cpp
@@ -362,7 +362,7 @@ namespace Aseba
 		
 		const Token::Type compoundAssignment[] = { Token::TOKEN_OP_ADD_EQUAL, Token::TOKEN_OP_NEG_EQUAL,
 							   Token::TOKEN_OP_MULT_EQUAL, Token::TOKEN_OP_DIV_EQUAL, Token::TOKEN_OP_MOD_EQUAL,
-							   Token::TOKEN_OP_BIT_OR_EQUAL, Token::TOKEN_OP_BIT_XOR_EQUAL, Token::TOKEN_OP_BIT_NOT_EQUAL,
+							   Token::TOKEN_OP_BIT_AND_EQUAL, Token::TOKEN_OP_BIT_OR_EQUAL, Token::TOKEN_OP_BIT_XOR_EQUAL, Token::TOKEN_OP_BIT_NOT_EQUAL,
 							   Token::TOKEN_OP_SHIFT_LEFT_EQUAL, Token::TOKEN_OP_SHIFT_RIGHT_EQUAL};
 		std::wstring varName = tokens.front().sValue;
 		SourcePos varPos = tokens.front().pos;