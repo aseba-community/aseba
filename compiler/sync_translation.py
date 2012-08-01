997ab9196a2308685426a232f4946965befe78f4
Florian Vaussard <florian.vaussard@epfl.ch>
2012-08-01 14:40:40 +0200

    Auto-generate errors_code.h from errors.cpp

--- compiler/sync_translation.py
+++ compiler/sync_translation.py
@@ -25,13 +25,45 @@ import sys
 from string import Template
 import re
 
+INPUT_DIRECTORY = '.'
 INPUT_FILE = 'errors.cpp'
-OUTPUT_FILE = 'CompilerTranslator.cpp'
-OUTPUT_DIRECTOR = '../studio'
+OUTPUT_CODE_FILE = 'errors_code.h'
+OUTPUT_CODE_DIRECTORY = INPUT_DIRECTORY
+OUTPUT_QT_FILE = 'CompilerTranslator.cpp'
+OUTPUT_QT_DIRECTORY = '../studio'
 
+comment_regexp = re.compile(r'\A\s*// (.*)')
 error_regexp = re.compile(r'error_map\[(.*?)\]\s*=\s*L"(.*?)";')
 
-output_file = \
+output_code_file = \
+"""
+#ifndef __ERRORS_H
+#define __ERRORS_H
+
+/*
+This file was automatically generated. Do not modify it,
+or your changes will be lost!
+You can manually run the script 'sync_translation.py'
+to generate a new version, based on errors.cpp
+*/
+
+// define error codes
+namespace Aseba
+{
+	enum ErrorCode
+	{
+		// ${start_comment}
+		${id_0} = 0,
+${id_others}
+		ERROR_END
+	};
+};
+
+#endif // __ERRORS_H
+"""
+output_code_file_template = Template(output_code_file)
+
+output_qt_file = \
 """
 #include "CompilerTranslator.h"
 #include "../compiler/errors_code.h"
@@ -60,18 +92,18 @@ ${elements}
 	}
 };
 """
-output_file_template = Template(output_file)
+output_qt_file_template = Template(output_qt_file)
 
-output_element = \
+output_qt_element = \
 """
 			case ${error_id}:
 				msg = tr("${error_msg}");
 				break;
 """
-output_element_template = Template(output_element)
+output_qt_element_template = Template(output_qt_element)
 
 # process input file
-filename = INPUT_FILE
+filename = os.path.join(INPUT_DIRECTORY, INPUT_FILE)
 print "Reading " + filename
 try:
     fh = file(filename)
@@ -81,22 +113,54 @@ except:
 
 case_vector = ""
 count = 0
+id_0 = None
+start_comment = ""
+id_others = ""
 
 print "Processing..."
 for line in fh:
+    # a comment?
+    match = comment_regexp.search(line)
+    if match:
+        # add it to the error codes file
+        if not id_0:
+            start_comment = match.group(1)
+        else:
+            id_others += "		// " + match.group(1) + "\n"
+        continue
+
     match = error_regexp.search(line)
     if match:
         count = count + 1
         code = match.group(1)
         msg = match.group(2)
-        case_vector += output_element_template.substitute(error_id=code, error_msg=msg)
+        # error codes file
+        if not id_0:
+            id_0 = code
+        else:
+            id_others += "		" + code + ",\n"
+        # qt file
+        case_vector += output_qt_element_template.substitute(error_id=code, error_msg=msg)
 
 fh.close()
 print "Found " + str(count) + " string(s)"
 
-result = output_file_template.substitute(elements=case_vector)
+# writing errors_code.h
+result = output_code_file_template.substitute(start_comment=start_comment, id_0=id_0, id_others=id_others)
+filename = os.path.join(OUTPUT_CODE_DIRECTORY, OUTPUT_CODE_FILE)
+print "Writing to " + filename
+try:
+    fh = file(filename, 'w')
+except:
+    print >> sys.stderr, "Invalid file " + filename
+    exit(1)
 
-filename = os.path.join(OUTPUT_DIRECTOR, OUTPUT_FILE)
+fh.write(result)
+fh.close()
+
+# writing the qt file
+result = output_qt_file_template.substitute(elements=case_vector)
+filename = os.path.join(OUTPUT_QT_DIRECTORY, OUTPUT_QT_FILE)
 print "Writing to " + filename
 try:
     fh = file(filename, 'w')
