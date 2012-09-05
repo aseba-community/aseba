29ea9cddef551bf6d965f1f1b17e1a9de68e075e
Florian Vaussard <florian.vaussard@epfl.ch>
2012-09-05 15:13:13 +0200

    maintainer/updatedoc: can update only one language if passed in cmd

--- maintainer/updatedoc/updatedoc.py
+++ maintainer/updatedoc/updatedoc.py
@@ -31,43 +31,74 @@ from wikidot.fetch import fetchwikidot
 import wikidot.structure
 import qthelp
 
-#language_map = [('fr','http://aseba.wikidot.com/fr:asebausermanual')]
-language_map = [
-                ('en','http://aseba.wikidot.com/en:asebausermanual'),
-                ('fr','http://aseba.wikidot.com/fr:asebausermanual'),
-                ('de','http://aseba.wikidot.com/de:asebausermanual'),
-                ('es','http://aseba.wikidot.com/es:asebausermanual')
-                ]
+complete_language_map = {
+                'en':'http://aseba.wikidot.com/en:asebausermanual',
+                'fr':'http://aseba.wikidot.com/fr:asebausermanual',
+                'de':'http://aseba.wikidot.com/de:asebausermanual',
+                'es':'http://aseba.wikidot.com/es:asebausermanual'
+                }                       # used for updating help index
+language_map = complete_language_map.copy()    # used for fetching
 OUTPUT_DIR = 'doc'
 QHP_FILE = './aseba-doc.qhp'
 CSS_FILE = './css/aseba.css'
 STUDIO_PATH = '../../studio/'
+STRUCTURE_CACHE = 'structure.cache'
 
-# Clean
-rmtree(OUTPUT_DIR, True)
-os.mkdir(OUTPUT_DIR)
+# Update only one language?
+if len(sys.argv) == 2:
+    lang = sys.argv[1]
+    url = None
+    try:
+        url = language_map[lang]
+    except KeyError:
+        print >> sys.stderr, "The language ", lang, " is not valid"
+        exit(1)
+    else:
+        # reset language map
+        language_map.clear()
+        language_map[lang] = url
+
+# Clean html/png/css
+if len(sys.argv) == 2:
+    # Clean only one language
+    lang = sys.argv[1]
+    print >> sys.stderr, "Performing a partial update"
+    files = os.listdir(OUTPUT_DIR)
+    for f in files:
+        if f.find(lang + '_') == 0:
+            os.remove(os.path.join(OUTPUT_DIR, f))
+else:
+    # Complete update
+    print >> sys.stderr, "Performing a complete update"
+    rmtree(OUTPUT_DIR, True)
+    os.mkdir(OUTPUT_DIR)
+
+# Clean qhp
 try:
     os.remove(QHP_FILE)
 except OSError:
     # File doesn't exist
     pass
 
+# Reload the cached structure
+try:
+    cache = open(STRUCTURE_CACHE, 'rb')
+    wikidot.structure.unserialize(cache)
+except IOError:
+    pass
+
 # Fetch the wiki, for all languages
-output_directories = list()
-available_languages = list()
-for x in language_map:
-    lang = x[0]
-    available_languages.append(lang)
+available_languages = language_map.keys()
+for lang in available_languages:
     print >> sys.stderr, "\n*** Getting doc for language ", lang, " ***"
     # Get + clean the output directory for the current language
     output = OUTPUT_DIR + '_' + lang
-    output_directories.append(output)
     rmtree(output, True)
     # Prepare the tree before fetching this language
     wikidot.structure.add_language(lang)
     wikidot.structure.set_current_language(lang)
     # Fetch!
-    fetchwikidot(x[1], output)
+    fetchwikidot(language_map[lang], output)
     # Add the CSS to output directory
     copy(CSS_FILE, output)
     # Copy from output to OUTPUT_DIR
@@ -76,8 +107,19 @@ for x in language_map:
     for y in listing:
         copy(os.path.join(output, y), os.path.join(OUTPUT_DIR, y))
 
+# Save the structure for future use
+cache = open(STRUCTURE_CACHE, 'wb')
+wikidot.structure.serialize(cache)
 
 # Generate the Qt Help files
+# Collect all the output directories
+output_directories = list()
+available_languages = list()
+for lang in complete_language_map.keys():
+    f = OUTPUT_DIR + '_' + lang
+    if os.path.isdir(f):
+        output_directories.append(f)
+        available_languages.append(lang)
 qthelp.generate(output_directories, OUTPUT_DIR, available_languages, QHP_FILE)
 
 # Clean Aseba Studio files
