6279aeb0e7ff2461dcad7f73c33878c1b5a9d1b3
Florian Vaussard <florian.vaussard@epfl.ch>
2013-04-02 09:10:55 +0200

    Japanese translations for challenge

--- targets/challenge/asebachallenge_ja.ts
+++ targets/challenge/asebachallenge_ja.ts
@@ -6,37 +6,37 @@
     <message>
         <location filename="challenge.cpp" line="877"/>
         <source>Aseba Challenge Help</source>
-        <translation type="unfinished"></translation>
+        <translation>Aseba Challengeのヘルプ</translation>
     </message>
     <message>
         <location filename="challenge.cpp" line="892"/>
         <source>Add a new robot</source>
-        <translation type="unfinished"></translation>
+        <translation>新しいロボットを追加</translation>
     </message>
     <message>
         <location filename="challenge.cpp" line="894"/>
         <source>Remove all robots</source>
-        <translation type="unfinished"></translation>
+        <translation>すべてのロボットを削除する</translation>
     </message>
     <message>
         <location filename="challenge.cpp" line="896"/>
         <source>Auto camera</source>
-        <translation type="unfinished"></translation>
+        <translation>自動カメラ</translation>
     </message>
     <message>
         <location filename="challenge.cpp" line="898"/>
         <source>Full screen</source>
-        <translation type="unfinished"></translation>
+        <translation>フルスクリーン</translation>
     </message>
     <message>
         <location filename="challenge.cpp" line="902"/>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>ヘルプ</translation>
     </message>
     <message>
         <location filename="challenge.cpp" line="904"/>
         <source>Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>終了</translation>
     </message>
 </context>
 <context>
@@ -44,12 +44,12 @@
     <message>
         <location filename="challenge.cpp" line="587"/>
         <source>Add a new robot</source>
-        <translation type="unfinished"></translation>
+        <translation>新しいロボットを追加</translation>
     </message>
     <message>
         <location filename="challenge.cpp" line="587"/>
         <source>Robot name:</source>
-        <translation type="unfinished"></translation>
+        <translation>ロボットの名前：</translation>
     </message>
 </context>
 <context>
@@ -57,17 +57,17 @@
     <message>
         <location filename="challenge.cpp" line="1071"/>
         <source>Please choose your language</source>
-        <translation type="unfinished"></translation>
+        <translation>言語を選ぶ</translation>
     </message>
     <message>
         <location filename="challenge.cpp" line="1091"/>
         <source>Ok</source>
-        <translation type="unfinished"></translation>
+        <translation>OK</translation>
     </message>
     <message>
         <location filename="challenge.cpp" line="1095"/>
         <source>Language selection</source>
-        <translation type="unfinished"></translation>
+        <translation>言語の選択</translation>
     </message>
 </context>
 <context>
@@ -75,12 +75,12 @@
     <message>
         <location filename="challenge.cpp" line="219"/>
         <source>Aseba Challenge</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="challenge.cpp" line="219"/>
         <source>Cannot create listening port %0: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>待ち受けポート（%0）を作成できません：%1</translation>
     </message>
 </context>
 </TS>
