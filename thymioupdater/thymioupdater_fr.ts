c01a5b5ad2a13ca2856222715056bd19e6a200a7
Stéphane Magnenat <stephane@magnenat.net>
2013-04-22 15:31:22 +0200

    Renammed thymioflasher into thymioupdater.

---
+++ thymioupdater/thymioupdater_fr.ts
@@ -0,0 +1,174 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr">
+<context>
+    <name>Aseba::ThymioFlasherDialog</name>
+    <message>
+        <source>Thymio Firmware Updater</source>
+        <translation type="obsolete">Mise à jour du firmware du Thymio</translation>
+    </message>
+    <message>
+        <source>Connection</source>
+        <translation type="obsolete">Connection</translation>
+    </message>
+    <message>
+        <source>Firmware file</source>
+        <translation type="obsolete">Fichier du firmware</translation>
+    </message>
+    <message>
+        <source>Select...</source>
+        <translation type="obsolete">Sélectionner...</translation>
+    </message>
+    <message>
+        <source>Update</source>
+        <translation type="obsolete">Mettre à jour</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation type="obsolete">Quitter</translation>
+    </message>
+    <message>
+        <source>Select hex file</source>
+        <translation type="obsolete">Séléctionner un fichier hex</translation>
+    </message>
+    <message>
+        <source>Hex files (*.hex)</source>
+        <translation type="obsolete">Fichiers hex (*.hex)</translation>
+    </message>
+    <message>
+        <source>Pre-update warning</source>
+        <translation type="obsolete">Alerte pré mise à jour</translation>
+    </message>
+    <message>
+        <source>Your are about to write a new firmware to the Thymio II. Make sure that the robot is charged and that the USB cable is properly connected.&lt;p&gt;&lt;b&gt;Do not unplug the robot during the update!&lt;/b&gt;&lt;/p&gt;Are you sure you want to proceed?</source>
+        <translation type="obsolete">Vous êtes sur le point d&apos;écrire un nouveau firmware dans le Thymio II. Assurez-vous que le robot est chargé et que le cable USB est connecté correctement.&lt;p&gt;&lt;b&gt;Ne déconnectez pas le robot durant la mise à jour !&lt;/b&gt;&lt;/p&gt;Êtes-vous sûr de vouloir continuer ?</translation>
+    </message>
+    <message>
+        <source>Cannot connect to target</source>
+        <translation type="obsolete">Ne peut pas se connecter à la destination</translation>
+    </message>
+    <message>
+        <source>Cannot connect to target: %1</source>
+        <translation type="obsolete">Ne peut pas se connecter à la destination : %1</translation>
+    </message>
+    <message>
+        <source>Update Error</source>
+        <translation type="obsolete">Erreur de mise à jour</translation>
+    </message>
+    <message>
+        <source>Unable to read Hex file: %1</source>
+        <translation type="obsolete">Ne peut pas lire le fichier hex : %1</translation>
+    </message>
+    <message>
+        <source>A bootloader error happened during the update process: %1</source>
+        <translation type="obsolete">Une erreur de bootloader est apparue durant le processus de mise à jour : %1</translation>
+    </message>
+    <message>
+        <source>A communication error happened during the update process: %1</source>
+        <translation type="obsolete">Une erreur de communication est survenue durant le processus de mise à jour : %1</translation>
+    </message>
+</context>
+<context>
+    <name>Aseba::ThymioUpdaterDialog</name>
+    <message>
+        <location filename="ThymioUpdater.cpp" line="61"/>
+        <source>Thymio Firmware Updater</source>
+        <translation>Mise à jour du firmware du Thymio</translation>
+    </message>
+    <message>
+        <location filename="ThymioUpdater.cpp" line="74"/>
+        <source>Firmware file</source>
+        <translation>Fichier du firmware</translation>
+    </message>
+    <message>
+        <location filename="ThymioUpdater.cpp" line="77"/>
+        <source>Select...</source>
+        <translation>Sélectionner...</translation>
+    </message>
+    <message>
+        <location filename="ThymioUpdater.cpp" line="92"/>
+        <source>Update</source>
+        <translation>Mettre à jour</translation>
+    </message>
+    <message>
+        <location filename="ThymioUpdater.cpp" line="95"/>
+        <source>Quit</source>
+        <translation>Quitter</translation>
+    </message>
+    <message>
+        <location filename="ThymioUpdater.cpp" line="122"/>
+        <source>Select hex file</source>
+        <translation>Séléctionner un fichier hex</translation>
+    </message>
+    <message>
+        <location filename="ThymioUpdater.cpp" line="122"/>
+        <source>Hex files (*.hex)</source>
+        <translation>Fichiers hex (*.hex)</translation>
+    </message>
+    <message>
+        <location filename="ThymioUpdater.cpp" line="130"/>
+        <source>Pre-update warning</source>
+        <translation>Alerte pré mise à jour</translation>
+    </message>
+    <message>
+        <location filename="ThymioUpdater.cpp" line="130"/>
+        <source>Your are about to write a new firmware to the Thymio II. Make sure that the robot is charged and that the USB cable is properly connected.&lt;p&gt;&lt;b&gt;Do not unplug the robot during the update!&lt;/b&gt;&lt;/p&gt;Are you sure you want to proceed?</source>
+        <translation>Vous êtes sur le point d&apos;écrire un nouveau firmware dans le Thymio II. Assurez-vous que le robot est chargé et que le cable USB est connecté correctement.&lt;p&gt;&lt;b&gt;Ne déconnectez pas le robot durant la mise à jour !&lt;/b&gt;&lt;/p&gt;Êtes-vous sûr de vouloir continuer ?</translation>
+    </message>
+    <message>
+        <location filename="ThymioUpdater.cpp" line="158"/>
+        <source>Cannot connect to target</source>
+        <translation>Ne peut pas se connecter à la destination</translation>
+    </message>
+    <message>
+        <location filename="ThymioUpdater.cpp" line="158"/>
+        <source>Cannot connect to target: %1</source>
+        <translation>Ne peut pas se connecter à la destination : %1</translation>
+    </message>
+    <message>
+        <location filename="ThymioUpdater.cpp" line="170"/>
+        <location filename="ThymioUpdater.cpp" line="174"/>
+        <location filename="ThymioUpdater.cpp" line="178"/>
+        <source>Update Error</source>
+        <translation>Erreur de mise à jour</translation>
+    </message>
+    <message>
+        <location filename="ThymioUpdater.cpp" line="170"/>
+        <source>Unable to read Hex file: %1</source>
+        <translation>Ne peut pas lire le fichier hex : %1</translation>
+    </message>
+    <message>
+        <location filename="ThymioUpdater.cpp" line="174"/>
+        <source>A bootloader error happened during the update process: %1</source>
+        <translation>Une erreur de bootloader est apparue durant le processus de mise à jour : %1</translation>
+    </message>
+    <message>
+        <location filename="ThymioUpdater.cpp" line="178"/>
+        <source>A communication error happened during the update process: %1</source>
+        <translation>Une erreur de communication est survenue durant le processus de mise à jour : %1</translation>
+    </message>
+</context>
+<context>
+    <name>QApplication</name>
+    <message>
+        <location filename="ThymioUpdater.cpp" line="241"/>
+        <source>Thymio II not found</source>
+        <translation>Thymio II non trouvé</translation>
+    </message>
+    <message>
+        <location filename="ThymioUpdater.cpp" line="241"/>
+        <source>&lt;p&gt;&lt;b&gt;Cannot find Thymio II!&lt;/b&gt;&lt;/p&gt;&lt;p&gt;Plug Thymio II or use the command-line updater.&lt;/p&gt;</source>
+        <translation>&lt;p&gt;&lt;b&gt;Ne peut pas trouver Thymio II !&lt;/b&gt;&lt;/p&gt;&lt;p&gt;Connectez Thymio II ou utilisez l&apos;outil de mise à jour en ligne de commande.&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="ThymioUpdater.cpp" line="246"/>
+        <source>Multiple Thymio II found</source>
+        <translation>Plusieurs Thymio II trouvés</translation>
+    </message>
+    <message>
+        <location filename="ThymioUpdater.cpp" line="246"/>
+        <source>&lt;p&gt;&lt;b&gt;More than one Thymio II found!&lt;/b&gt;&lt;/p&gt;&lt;p&gt;Plug a single Thymio II or use the command-line updater.&lt;/p&gt;</source>
+        <translation>&lt;p&gt;&lt;b&gt;Plusieurs Thymio II trouvés !&lt;/b&gt;&lt;/p&gt;&lt;p&gt;Connectez un seul Thymio II ou utilisez l&apos;outil de mise à jour en ligne de commande.&lt;/p&gt;</translation>
+    </message>
+</context>
+</TS>
