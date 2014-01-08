8e755f03ff73ce3bb8af8d2813230e8849a6df7c
Stéphane Magnenat <stephane@magnenat.net>
2014-01-08 14:11:47 +0100

    Bumped version

--- common/consts.h
+++ common/consts.h
@@ -27,10 +27,10 @@
 /*@{*/
 
 /*! version of Aseba as string */
-#define ASEBA_VERSION "1.3.1"
+#define ASEBA_VERSION "1.3.2"
 
 /*! version of Aseba as an int */
-#define ASEBA_VERSION_INT 10301
+#define ASEBA_VERSION_INT 10302
 
 /*! version of aseba protocol, including bytecodes types and constants */
 #define ASEBA_PROTOCOL_VERSION 4
