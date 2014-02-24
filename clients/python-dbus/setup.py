ad7dcc5377f1c20e8119bc517b283d8eb9972ce3
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2014-02-24 14:54:45 +0100

    Added a setup.py for the python aseba bindings

---
+++ clients/python-dbus/setup.py
@@ -0,0 +1,14 @@
+ #!/usr/bin/env python
+ # -*- coding: utf-8 -*-
+
+import os
+from distutils.core import setup
+
+setup(name='aseba',
+      version='1.0',
+      license='LGPL-v3',
+      description='A DBus-based Python binding for Aseba',
+      author='Séverin Lemaignan, Stéphane Magnenat',
+      author_email='severin.lemaignan@epfl.ch',
+      py_modules=['aseba'],
+      )
