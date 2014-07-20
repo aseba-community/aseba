15ff49d7bbc5870cb5135f10259b5020a60294a9
Stéphane Magnenat <stephane@magnenat.net>
2014-07-20 19:33:19 +0200

    Changed version number for master to make clear we now are in 1.4.0 alpha.

--- common/consts.h
+++ common/consts.h
@@ -27,10 +27,10 @@
 /*@{*/
 
 /*! version of Aseba as string */
-#define ASEBA_VERSION "1.3.1"
+#define ASEBA_VERSION "1.3.98"
 
 /*! version of Aseba as an int */
-#define ASEBA_VERSION_INT 10301
+#define ASEBA_VERSION_INT 10398
 
 /*! version of aseba protocol, including bytecodes types and constants */
 #define ASEBA_PROTOCOL_VERSION 4
