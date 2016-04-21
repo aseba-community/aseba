7c0f8cbbabfd67ac0ced09dd8c72d5940204ffc3
Stéphane Magnenat <stephane@magnenat.net>
2016-04-21 15:47:34 +0200

    Updated version number.

--- common/consts.h
+++ common/consts.h
@@ -27,10 +27,10 @@
 /*@{*/
 
 /*! version of Aseba as string */
-#define ASEBA_VERSION "1.5.2"
+#define ASEBA_VERSION "1.5.3"
 
 /*! version of Aseba as an int */
-#define ASEBA_VERSION_INT 10502
+#define ASEBA_VERSION_INT 10503
 
 /*! version of aseba protocol, including bytecodes types and constants */
 #define ASEBA_PROTOCOL_VERSION 5
