26ab4f17c3ce4b1661556ce883d82f5eb39682df
Stéphane Magnenat <stephane@magnenat.net>
2016-08-18 09:33:44 +0200

    Added missing files

---
+++ switches/http2/Switch.cpp
@@ -0,0 +1,57 @@
+/*
+	Aseba - an event-based framework for distributed robot control
+	Copyright (C) 2007--2016:
+		Stephane Magnenat <stephane at magnenat dot net>
+		(http://stephane.magnenat.net)
+		and other contributors, see authors.txt for details
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published
+	by the Free Software Foundation, version 3 of the License.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "Switch.h"
+
+namespace Aseba
+{
+	Switch::NodeWithProgram::NodeWithProgram(const TargetDescription& targetDescription):
+		Node(targetDescription)
+	{
+	}
+	
+	void Switch::NodeWithProgram::compile(const CommonDefinitions& commonDefinitions)
+	{
+		Compiler compiler;
+		compiler.setTargetDescription(this);
+		compiler.setCommonDefinitions(&commonDefinitions);
+		
+		std::wistringstream is(code);
+		
+		success = compiler.compile(is, bytecode, allocatedVariablesCount, error);
+		
+		if (success)
+		{
+			variables = compiler.getVariablesMap();
+			subroutinesNames = compiler.getSubroutinesNames();
+		}
+	}
+	
+	Switch::Node* Switch::createNode(const TargetDescription& targetDescription)
+	{
+		return new NodeWithProgram(targetDescription);
+	}
+	
+	void Switch::sendMessage(const Message& message)
+	{
+		// TODO: implement
+	}
+	
+} // namespace Aseba
---
+++ switches/http2/Switch.h
@@ -0,0 +1,56 @@
+/*
+	Aseba - an event-based framework for distributed robot control
+	Copyright (C) 2007--2016:
+		Stephane Magnenat <stephane at magnenat dot net>
+		(http://stephane.magnenat.net)
+		and other contributors, see authors.txt for details
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published
+	by the Free Software Foundation, version 3 of the License.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef ASEBA_SWITCH_SWITCH
+#define ASEBA_SWITCH_SWITCH
+
+#include <dashel/dashel.h>
+#include "../../common/msg/NodesManager.h"
+
+namespace Aseba
+{
+	class Switch: public Dashel::Hub, public NodesManager
+	{
+	protected:
+		//! A node that has a program and the result of compilation attached
+		struct NodeWithProgram: public Node, public CompilationResult
+		{
+			NodeWithProgram(const TargetDescription& targetDescription);
+			
+			void compile(const CommonDefinitions& commonDefinitions);
+			
+			std::wstring code; //!< the source code of the program
+		};
+		
+	protected:
+		// from NodesManager
+		
+		virtual Node* createNode(const TargetDescription& targetDescription);
+		virtual void sendMessage(const Message& message);
+		
+		
+	protected:
+		CommonDefinitions commonDefinitions; //!< global events and constants, user-definable
+		
+	};
+	
+} // namespace Aseba
+
+#endif // ASEBA_SWITCH_SWITCH
