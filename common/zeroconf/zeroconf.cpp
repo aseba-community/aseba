95b8651927d3cd282e10c7dcc468f4a4b0d78fd5
davidjsherman <david.sherman@inria.fr>
2016-09-11 23:48:52 +0200

    Aseba::Zeroconf provides Zeroconf (mDNS-SD) services

---
+++ common/zeroconf/zeroconf.cpp
@@ -0,0 +1,224 @@
+/*
+	Aseba - an event-based framework for distributed robot control
+	Copyright (C) 2007--2016:
+		Stephane Magnenat <stephane at magnenat dot net>
+		(http://stephane.magnenat.net)
+		and other contributors, see authors.txt for details
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published
+	by the Free Software Foundation, version 3 of the License.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "../utils/utils.h"
+#include "../utils/FormatableString.h"
+#include <dashel/dashel.h>
+#include "zeroconf.h"
+
+using namespace std;
+
+namespace Aseba
+{
+	using namespace Dashel;
+
+	//! Create a target in the Zeroconf container, described by a name and a port
+	Zeroconf::Target& Zeroconf::insert(const std::string & name, const int & port)
+	{
+		targets.push_back({name,port});
+		targets.back().container = this;
+		return targets.back();
+	}
+
+	//! Create a target in the Zeroconf container, described by a Dashel stream
+	Zeroconf::Target& Zeroconf::insert(const Dashel::Stream* dashel_stream)
+	{
+		targets.push_back({dashel_stream});
+		targets.back().container = this;
+		return targets.back();
+	}
+
+	//! Search the Zeroconf container by human-readable target name
+	std::vector<Zeroconf::Target>::iterator Zeroconf::find(const std::string & name)
+	{
+		return find_if(targets.begin(),
+			       targets.end(),
+			       [&] (const Zeroconf::Target& t) { return t.name.find(name) == 0; } );
+	}
+
+	// Overideable method to wait for responses from the DNS service.
+	// Basic behavior is to block until daemon replies.
+	// The serviceref is released by callbacks.
+	void Zeroconf::processDiscoveryRequest(ZeroconfDiscoveryRequest & zdr)
+	{
+		while (zdr.in_process) {
+			DNSServiceErrorType err = DNSServiceProcessResult(zdr.serviceref);
+			if (err != kDNSServiceErr_NoError)
+				throw Zeroconf::Error(FormatableString("DNSServiceProcessResult: error %0").arg(err));
+		}
+	}
+
+	//! A target can ask Zeroconf to register it in DNS, with additional information in a TXT record
+	void Zeroconf::registerTarget(Zeroconf::Target * target, const TxtRecord& txtrec)
+	{
+		string txt{txtrec.record()};
+		uint16_t len = txt.size();
+		const char* record = txt.c_str();
+		target->zdr.~ZeroconfDiscoveryRequest();
+		auto err = DNSServiceRegister(&(target->zdr.serviceref),
+					      kDNSServiceFlagsDefault,
+					      0, // default all interfaces
+					      target->name.c_str(),
+					      "_aseba._tcp",
+					      NULL, // use default domain, usually "local."
+					      NULL, // use this host name
+					      htons(target->port),
+					      len, // TXT length
+					      record, // TXT record
+					      cb_Register,
+					      target); // context pointer is Zeroconf::Target object
+		if (err != kDNSServiceErr_NoError)
+			throw Zeroconf::Error(FormatableString("DNSServiceRegister: error %0").arg(err));
+		else
+			processDiscoveryRequest(target->zdr);
+	}
+
+	//! DNSSD callback for registerTarget, update Zeroconf::Target record with results of registration
+	void DNSSD_API Zeroconf::cb_Register(DNSServiceRef sdRef,
+					     DNSServiceFlags flags,
+					     DNSServiceErrorType errorCode,
+					     const char *name,
+					     const char *regtype,
+					     const char *domain,
+					     void *context)
+	{
+		if (errorCode != kDNSServiceErr_NoError)
+			throw Zeroconf::Error(FormatableString("DNSServiceRegisterReply: error %0").arg(errorCode));
+		else
+		{
+			Zeroconf::Target *target = static_cast<Zeroconf::Target *>(context);
+
+			target->name = name;
+			target->domain = domain;
+			target->zdr.in_process = false;
+		}
+	}
+
+
+	//! A target can ask Zeroconf to update its TXT record
+	void Zeroconf::updateTarget(const Zeroconf::Target * target, const TxtRecord& txtrec)
+	{
+		const string rawdata{txtrec.record()};
+		DNSServiceErrorType err = DNSServiceUpdateRecord(target->zdr.serviceref,
+								 NULL,
+								 NULL,
+								 rawdata.length(),
+								 rawdata.c_str(),
+								 0);
+		if (err != kDNSServiceErr_NoError)
+			throw Zeroconf::Error(FormatableString("DNSServiceUpdateRecord: error %0").arg(err));
+	}
+
+	//! A remote target can ask Zeroconf to resolve its host name and port
+	void Zeroconf::resolveTarget(Zeroconf::Target * target)
+	{
+		target->zdr.~ZeroconfDiscoveryRequest();
+		auto err = DNSServiceResolve(&(target->zdr.serviceref),
+						 kDNSServiceFlagsForceMulticast,
+						 0,
+						 target->name.c_str(),
+						 target->regtype.c_str(),
+						 target->domain.c_str(),
+						 cb_Resolve,
+						 target);
+		if (err != kDNSServiceErr_NoError)
+			throw Zeroconf::Error(FormatableString("DNSServiceQueryRecord: error %0").arg(err));
+		else
+			processDiscoveryRequest(target->zdr);
+	}
+
+	//! DNSSD callback for resolveTarget, update Zeroconf::Target record with results of lookup
+	void DNSSD_API Zeroconf::cb_Resolve(DNSServiceRef sdRef,
+					    DNSServiceFlags flags,
+					    uint32_t interfaceIndex,
+					    DNSServiceErrorType errorCode,
+					    const char * fullname,
+					    const char * hosttarget,
+					    uint16_t port,
+					    uint16_t txtLen,
+					    const unsigned char * txtRecord,
+					    void *context)
+	{
+		if (errorCode != kDNSServiceErr_NoError)
+			throw Zeroconf::Error(FormatableString("DNSServiceRegisterReply: error %0").arg(errorCode));
+		else
+		{
+			Zeroconf::Target *target = static_cast<Zeroconf::Target *>(context);
+
+			target->host = hosttarget;
+			target->port = ntohs(port);
+			Aseba::Zeroconf::TxtRecord tnew{ txtRecord,txtLen };
+			target->properties.clear();
+			for (auto const & field: tnew)
+				target->properties[field.first] = field.second;
+			target->properties["fullname"] = string(fullname);
+			target->zdr.in_process = false;
+		}
+	}
+
+	//! Update the set of known targets with remote ones found by browsing DNS
+	void Zeroconf::browse()
+	{
+		// remove previously discovered targets
+		targets.erase(std::remove_if(targets.begin(),targets.end(),
+					     [](const Target& t) { return ! t.local; }),
+			      targets.end());
+		browseZDR.~ZeroconfDiscoveryRequest();
+		auto err = DNSServiceBrowse(&browseZDR.serviceref,
+					    kDNSServiceFlagsDefault,
+					    0, // default all interfaces
+					    "_aseba._tcp",
+					    NULL, // use default domain, usually "local."
+					    cb_Browse,
+					    this); // context pointer is this Zeroconf object
+		if (err != kDNSServiceErr_NoError)
+			throw Zeroconf::Error(FormatableString("DNSServiceRegister: error %0").arg(err));
+		else
+			processDiscoveryRequest(browseZDR);
+	}
+
+	//! DNSSD callback for browse, update ZeroconfDiscoveryRequest record with results of browse
+	void DNSSD_API Zeroconf::cb_Browse(DNSServiceRef sdRef,
+					   DNSServiceFlags flags,
+					   uint32_t interfaceIndex,
+					   DNSServiceErrorType errorCode,
+					   const char *name,
+					   const char *regtype,
+					   const char *domain,
+void *context)
+	{
+		if (errorCode != kDNSServiceErr_NoError)
+			throw Zeroconf::Error(FormatableString("DNSServiceBrowseReply: error %0").arg(errorCode));
+		else
+		{
+			Zeroconf *zref = static_cast<Zeroconf *>(context);
+			if (flags & kDNSServiceFlagsAdd)
+			{
+				auto it = zref->find(name);
+				auto target = (it == zref->targets.end()) ? zref->insert(string(name), 0) : *it; // since port==0 at creation it will be marked as nonlocal
+				target.properties = { {"name",string(name)}, {"domain",string(domain)} };
+			}
+			if ( ! (flags & kDNSServiceFlagsMoreComing))
+				zref->browseZDR.in_process = false;
+				//zref->browseZDR = ZeroconfDiscoveryRequest{}; // erase serviceref and terminate browsing
+		}
+	}
+
+} // namespace Aseba
---
+++ common/zeroconf/zeroconf.h
@@ -0,0 +1,210 @@
+/*
+	Aseba - an event-based framework for distributed robot control
+	Copyright (C) 2007--2016:
+		Stephane Magnenat <stephane at magnenat dot net>
+		(http://stephane.magnenat.net)
+		and other contributors, see authors.txt for details
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published
+	by the Free Software Foundation, version 3 of the License.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ASEBA_ZEROCONF
+#define ASEBA_ZEROCONF
+
+#include "../types.h"
+#include "../consts.h"
+#include <typeinfo>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <vector>
+#include <thread>
+#include <dns_sd.h>
+
+namespace Dashel
+{
+	class Stream;
+}
+
+namespace Aseba
+{
+	/**
+	 \defgroup zeroconf Zeroconf (mDNS-SD multicast DNS Service Discovery)
+
+		Using Zeroconf, an Aseba target announces itself on the TCP/IP network
+		as a service of type _aseba._tcp, and can be discovered by clients.
+		A target might be an individual node or an Aseba switch.
+		The zeroconf service provides minimal information about a target:
+		Dashel target (host name, IP port), target name, target type, Aseba
+		protocol version, a list of node ids, and a list of node product ids.
+		A client may obtain complete node descriptions by connecting to the
+		Dashel target and requesting them using the Aseba protocol
+	 */
+	/*@{*/
+
+	//! Aseba::Zeroconf provides methods for registering targets, updating target
+	//! descriptions, and browsing for targets. It runs a thread that watches the
+	//! DNS Service for updates, and triggers callback processing as necessary.
+	class Zeroconf
+	{
+	public:
+		class TxtRecord;
+		class Target;
+//		typedef std::map<std::string, Zeroconf::Target*> NameTargetMap;
+
+	public:
+		//! Aseba::Zeroconf is a container of targets
+		virtual void erase(std::vector<Target>::iterator position) { targets.erase(position); }
+		virtual void clear() { targets.clear(); }
+		virtual bool empty() { return targets.empty(); }
+		virtual int size() { return targets.size(); }
+		virtual Target & front() { return targets.front(); }
+		virtual std::vector<Target>::iterator begin() { return targets.begin(); }
+		virtual std::vector<Target>::iterator end() { return targets.end(); }
+		virtual std::vector<Target>::iterator find(const std::string & name);
+
+		//! Aseba::Zeroconf is a factory for creating and inserting targets
+		virtual Target& insert(const std::string & name, const int & port);
+		virtual Target& insert(const Dashel::Stream* dashel_stream);
+
+		//! Aseba::Zeroconf can update its knowledge of non-local targets by browsing the network
+		virtual void browse();
+
+	protected:
+		std::vector<Target> targets; //!< the targets in this container
+		void registerTarget(Target * target, const TxtRecord& txtrec); //!< requested through target
+		void updateTarget(const Target * target, const TxtRecord& txtrec); //!< requested through target
+		void resolveTarget(Target * target); //!< requested through target
+
+	public:
+		//! An error in registering or browsing Zeroconf
+		struct Error:public std::runtime_error
+		{
+			Error(const std::string& what): std::runtime_error(what) {}
+		};
+
+	protected:
+		//! Private class that encapsulates an active service request to the mDNS-SD daemon
+		class ZeroconfDiscoveryRequest
+		{
+		public:
+			DNSServiceRef serviceref{nullptr};
+			bool in_process{true}; //!< flag to signal when this request is no longer active
+		public:
+			virtual ~ZeroconfDiscoveryRequest()
+			{   // release the service reference
+				if (serviceref)
+					DNSServiceRefDeallocate(serviceref);
+				serviceref = 0;
+			}
+			virtual bool operator==(const ZeroconfDiscoveryRequest &other) const
+			{
+				return serviceref == other.serviceref;
+			}
+		};
+
+		ZeroconfDiscoveryRequest browseZDR; //! the zdr for browse requests isn't attached to a target
+
+	protected:
+		//! Called when the browsing process is complete.
+		//! Can be overridden in derived classes to schedule an update to the UI.
+		virtual void browseComplete() {}
+
+		//! The discovery request can be processed immediately, or can be registered with
+		//! an event loop for asynchronous processing.
+		//! Can be overridden in derived classes to set up asynchronous processing.
+		virtual void processDiscoveryRequest(ZeroconfDiscoveryRequest & zdr);
+
+	private:
+		//! callbacks for the DNS Service Discovery API
+		static void DNSSD_API cb_Register(DNSServiceRef sdRef, DNSServiceFlags flags, DNSServiceErrorType errorCode, const char *name, const char *regtype, const char *domain, void *context);
+		static void DNSSD_API cb_Browse(DNSServiceRef sdRef, DNSServiceFlags flags, uint32_t interfaceIndex, DNSServiceErrorType errorCode, const char *serviceName, const char *regtype, const char *replyDomain, void *context);
+		static void DNSSD_API cb_Resolve(DNSServiceRef sdRef, DNSServiceFlags flags, uint32_t interfaceIndex, DNSServiceErrorType errorCode, const char *fullname, const char *hosttarget, uint16_t port, /* In network byte order */ uint16_t txtLen, const unsigned char *txtRecord, void *context);
+	};
+
+	/**
+	 \addtogroup zeroconf
+
+		A Zeroconf::Target allows client classes to choose and access Aseba targets.
+	 */
+	class Zeroconf::Target
+	{
+	public:
+		std::string name;
+		int port;
+		std::string domain{"local."};
+		std::string regtype{"_aseba._tcp"};
+		std::string host{"localhost"};
+		bool local{true};
+		ZeroconfDiscoveryRequest zdr;
+
+	public:
+		Target(const std::string & name, const int & port);
+		Target(const Dashel::Stream* dashel_stream);
+		virtual void advertise(const TxtRecord& txtrec); //!< Inform the DNS service about this target
+		virtual void updateTxtRecord(const TxtRecord& txtrec); //!< Update this target's description in the DNS service
+		virtual void resolve(); //!< Ask the DNS service for the host name and port of this target
+
+		std::map<std::string, std::string> properties; //!< User-modifiable metadata about this target
+
+		virtual bool operator==(const Target &other) const
+		{
+			return name == other.name && domain == other.domain;
+		}
+
+	private:
+		friend Zeroconf;
+		Zeroconf * container; //!< Back reference to containing Aseba::Zeroconf object
+	};
+
+	/**
+	 \addtogroup zeroconf
+
+		Zeroconf records three DNS records for each service: a PTR for the
+		symbolic name, an SRV for the actual host and port number, and a TXT
+		for extra information. The TXT record is used by clients to help choose
+		the hosts it wishes to connect to. The syntax of TXT records is defined
+		in https://tools.ietf.org/html/rfc6763#section-6.
+		The TXT record for Aseba targets specifies the target type, the Aseba
+		protocol version, a list of node ids, and a list of node product ids.
+	 */
+
+	class Zeroconf::TxtRecord
+	{
+	public:
+		TxtRecord(const unsigned int protovers, const std::string& type, const std::vector<unsigned int>& ids, const std::vector<unsigned int>& pids);
+		TxtRecord(const std::string & txtRecord);
+		TxtRecord(const unsigned char * txtRecord, uint16_t txtLen);
+		//! Different kinds of keys can be encoded into TXT records
+		virtual void assign(const std::string& key, const std::string& value);
+		virtual void assign(const std::string& key, const int value);
+		virtual void assign(const std::string& key, const std::vector<unsigned int>& values);
+		virtual void assign(const std::string& key, const bool value);
+		//! Retrieve the encoded TXT record
+		virtual std::string record() const;
+
+		virtual const std::string at (const std::string& k) const { return fields.at(k); }
+		virtual std::map<std::string, std::string>::iterator begin() { return fields.begin(); }
+		virtual std::map<std::string, std::string>::iterator end() { return fields.end(); }
+
+	private:
+		std::map<std::string, std::string> fields;
+		virtual void serialize_field(std::ostringstream& txt, const std::string& key) const; //!< serialize a field into the encoded TXT record
+	};
+
+	/*@}*/
+} // namespace Aseba
+
+#endif /* ASEBA_ZEROCONF */
