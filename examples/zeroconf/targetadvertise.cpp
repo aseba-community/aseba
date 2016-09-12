ce1c8e2972f39d5177167a08cf96f9391fe586b7
davidjsherman <david.sherman@inria.fr>
2016-09-12 10:29:08 +0200

    #include <string.h> for strncmp

--- examples/zeroconf/targetadvertise.cpp
+++ examples/zeroconf/targetadvertise.cpp
@@ -18,6 +18,7 @@
 	along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <string.h>
 #include <dashel/dashel.h>
 #include "../../common/productids.h"
 #include "../../common/consts.h"
