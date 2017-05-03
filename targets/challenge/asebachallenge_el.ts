4df9078629363142c77596f19d8a28d089b012ff
Ioanna Theodoropoulou <ioannatheodoropou@hotmail.com>
2017-05-03 09:37:39 +0300

    Update asebachallenge_el.ts

--- targets/challenge/asebachallenge_el.ts
+++ targets/challenge/asebachallenge_el.ts
@@ -6,37 +6,37 @@
     <message>
         <location filename="challenge.cpp" line="889"/>
         <source>Aseba Challenge Help</source>
-        <translation type="unfinished"></translation>
+        <translation>Βοήθεια Aseba Challenge</translation>
     </message>
     <message>
         <location filename="challenge.cpp" line="904"/>
         <source>Add a new robot</source>
-        <translation type="unfinished"></translation>
+        <translation>Προσθήκη νέου ρομπότ</translation>
     </message>
     <message>
         <location filename="challenge.cpp" line="906"/>
         <source>Remove all robots</source>
-        <translation type="unfinished"></translation>
+        <translation>Αφαίρεση όλων των ρομπότ</translation>
     </message>
     <message>
         <location filename="challenge.cpp" line="908"/>
         <source>Auto camera</source>
-        <translation type="unfinished"></translation>
+        <translation>Αυτόματη κάμερα</translation>
     </message>
     <message>
         <location filename="challenge.cpp" line="910"/>
         <source>Full screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Πλήρης οθόνη</translation>
     </message>
     <message>
         <location filename="challenge.cpp" line="914"/>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>Βοήθεια</translation>
     </message>
     <message>
         <location filename="challenge.cpp" line="916"/>
         <source>Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>Έξοδος</translation>
     </message>
 </context>
 <context>
@@ -44,12 +44,12 @@
     <message>
         <location filename="challenge.cpp" line="597"/>
         <source>Add a new robot</source>
-        <translation type="unfinished"></translation>
+        <translation>Προσθήκη νέου ρομπότ</translation>
     </message>
     <message>
         <location filename="challenge.cpp" line="597"/>
         <source>Robot name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Όνομα ρομπότ:</translation>
     </message>
 </context>
 <context>
@@ -57,17 +57,17 @@
     <message>
         <location filename="challenge.cpp" line="1083"/>
         <source>Please choose your language</source>
-        <translation type="unfinished"></translation>
+        <translation>Παρακαλώ επιλέξτε τη γλώσσα σας</translation>
     </message>
     <message>
         <location filename="challenge.cpp" line="1106"/>
         <source>Ok</source>
-        <translation type="unfinished"></translation>
+        <translation>ΟΚ</translation>
     </message>
     <message>
         <location filename="challenge.cpp" line="1110"/>
         <source>Language selection</source>
-        <translation type="unfinished"></translation>
+        <translation>Επιλογή γλώσσας</translation>
     </message>
 </context>
 <context>
@@ -75,12 +75,12 @@
     <message>
         <location filename="challenge.cpp" line="221"/>
         <source>Aseba Challenge</source>
-        <translation type="unfinished"></translation>
+        <translation>Aseba Challenge</translation>
     </message>
     <message>
         <location filename="challenge.cpp" line="221"/>
         <source>Cannot create listening port %0: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Αποτυχία δημιουργίας θύρας ακρόασης %0: %1</translation>
     </message>
 </context>
 </TS>
