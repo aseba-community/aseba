443eb7dac6406f15eab5f1315666cb0e6c1f3ecb
Ioanna Theodoropoulou <ioannatheodoropou@hotmail.com>
2017-09-15 22:12:57 +0300

    Update thymiownetconfig_el.ts

--- clients/thymiownetconfig/thymiownetconfig_el.ts
+++ clients/thymiownetconfig/thymiownetconfig_el.ts
@@ -6,73 +6,73 @@
     <message>
         <location filename="ThymioWNetConfig.cpp" line="85"/>
         <source>Wireless Thymio Network Configurator</source>
-        <translation type="unfinished"></translation>
+        <translation>Ρύθμιση Δικτύου Ασύρματου Thymio</translation>
     </message>
     <message>
         <location filename="ThymioWNetConfig.cpp" line="104"/>
         <source>0</source>
-        <translation type="unfinished"></translation>
+        <translation>0</translation>
     </message>
     <message>
         <location filename="ThymioWNetConfig.cpp" line="105"/>
         <source>1</source>
-        <translation type="unfinished"></translation>
+        <translation>1</translation>
     </message>
     <message>
         <location filename="ThymioWNetConfig.cpp" line="106"/>
         <source>2</source>
-        <translation type="unfinished"></translation>
+        <translation>2</translation>
     </message>
     <message>
         <location filename="ThymioWNetConfig.cpp" line="108"/>
         <source>Channel:</source>
-        <translation type="unfinished"></translation>
+        <translation>Κανάλι:</translation>
     </message>
     <message>
         <location filename="ThymioWNetConfig.cpp" line="120"/>
         <source>Network identifier:</source>
-        <translation type="unfinished"></translation>
+        <translation>Αναγνωριστικό Δικτύου:</translation>
     </message>
     <message>
         <location filename="ThymioWNetConfig.cpp" line="130"/>
         <source>Dongle node identifier:</source>
-        <translation type="unfinished"></translation>
+        <translation>Αναγνωριστικό κόμβου πομποδέκτη (dongle):</translation>
     </message>
     <message>
         <location filename="ThymioWNetConfig.cpp" line="137"/>
         <location filename="ThymioWNetConfig.cpp" line="227"/>
         <source>Enable pairing</source>
-        <translation type="unfinished"></translation>
+        <translation>Ενεργοποίηση αντιστοίχισης</translation>
     </message>
     <message>
         <location filename="ThymioWNetConfig.cpp" line="138"/>
         <source>Flash into dongle</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Αποθήκευση στον πομποδέκτη (dongle)</translation>
     </message>
     <message>
         <location filename="ThymioWNetConfig.cpp" line="139"/>
         <source>Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>Ακύρωση</translation>
     </message>
     <message>
         <location filename="ThymioWNetConfig.cpp" line="158"/>
         <source>Wireless dongle firmware version %0</source>
-        <translation type="unfinished"></translation>
+        <translation>Έκδοση υλικολογισμικού ασύρματου πομποδέκτη (dongle) %0</translation>
     </message>
     <message>
         <location filename="ThymioWNetConfig.cpp" line="171"/>
         <source>Connection error</source>
-        <translation type="unfinished"></translation>
+        <translation>Σφάλμα σύνδεσης</translation>
     </message>
     <message>
         <location filename="ThymioWNetConfig.cpp" line="171"/>
         <source>&lt;p&gt;&lt;b&gt;Cannot connect to dongle!&lt;/b&gt;&lt;/p&gt;&lt;p&gt;Make sure a Wireless Thymio dongle is connected!&lt;/p&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;&lt;b&gt;Δεν είναι δυνατή η σύνδεση με τον πομποδέκτη (dongle)!&lt;/b&gt;&lt;/p&gt;&lt;p&gt;Βεβαιωθείτε ότι είναι συνδεδεμένος ένας πομποδέκτης (dongle) Ασύρματου Thymio!&lt;/p&gt;</translation>
     </message>
     <message>
         <location filename="ThymioWNetConfig.cpp" line="223"/>
         <source>Disable pairing</source>
-        <translation type="unfinished"></translation>
+        <translation>Απενεργοποίηση αντιστοίχισης</translation>
     </message>
 </context>
 <context>
@@ -80,22 +80,22 @@
     <message>
         <location filename="ThymioWNetConfig.cpp" line="274"/>
         <source>Wireless Thymio dongle not found</source>
-        <translation type="unfinished"></translation>
+        <translation>Δε βρέθηκε πομποδέκτης (dongle) Ασύρματου Thymio</translation>
     </message>
     <message>
         <location filename="ThymioWNetConfig.cpp" line="274"/>
         <source>&lt;p&gt;&lt;b&gt;Cannot find a Wireless Thymio dongle!&lt;/b&gt;&lt;/p&gt;&lt;p&gt;Plug a dongle into one of your USB ports and try again.&lt;/p&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;&lt;b&gt;Δεν είναι δυνατή η εύρεση πομποδέκτη (dongle) Ασύρματου Thymio!&lt;/b&gt;&lt;/p&gt;&lt;p&gt;Συνδέστε έναν πομποδέκτη (dongle) σε μία από τις USB θύρες σας και προσπαθήστε ξανά.&lt;/p&gt;</translation>
     </message>
     <message>
         <location filename="ThymioWNetConfig.cpp" line="279"/>
         <source>Multiple Wireless Thymio dongles found</source>
-        <translation type="unfinished"></translation>
+        <translation>Βρέθηκαν πολλοί πομποδέκτες (dongles) Ασύρματου Thymio</translation>
     </message>
     <message>
         <location filename="ThymioWNetConfig.cpp" line="279"/>
         <source>&lt;p&gt;&lt;b&gt;More than one Wireless Thymio dongles found!&lt;/b&gt;&lt;/p&gt;&lt;p&gt;Plug a single dongle into your computer and try again.&lt;/p&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;&lt;b&gt;Βρέθηκαν περισσότεροι από ένας πομποδέκτες (dongles) Ασύρματου Thymio!&lt;/b&gt;&lt;/p&gt;&lt;p&gt;Συνδέστε μόνο έναν πομποδέκτη (dongle) στον υπολογιστή σας και προσπαθήστε ξανά.&lt;/p&gt;</translation>
     </message>
 </context>
 </TS>
