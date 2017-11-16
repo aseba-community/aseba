16d84c112d5e1790a7d0b0389914314fe782e015
Stephane Magnenat <stephane@magnenat.net>
2017-11-16 09:58:33 +0100

    Move definition of inner types of Zeroconf to fix compilation with gcc 7

--- common/zeroconf/zeroconf.h
+++ common/zeroconf/zeroconf.h
@@ -63,14 +63,71 @@ namespace Aseba
 	class Zeroconf
 	{
 	public:
-		struct Error;
-		struct TargetKey;
-		struct TargetInformation;
-		class Target;
-		class TxtRecord;
+		//! An error in registering or browsing Zeroconf
+		struct Error: public std::runtime_error
+		{
+			Error(const std::string & what): std::runtime_error(what) {}
+		};
+
+		//! A TargetKey allows to uniquely identify a target on the network within Zeroconf.
+		//! It contains the informations returned by browse().
+		struct TargetKey
+		{
+			std::string name; //!< the full name of this target
+			std::string regtype{"_aseba._tcp"}; //!< the mDNS registration type
+			std::string domain{"local."}; //!< the domain of the host
+		};
+
+		//! A TargetInformation allows client classes to choose and access Aseba targets.
+		struct TargetInformation: TargetKey
+		{
+			// metadata about this target after resolution
+			std::string host; //!< the Dashel target connection ports
+			int port{0}; //!< the Dashel target connection port
+
+			std::map<std::string, std::string> properties; //!< user-modifiable metadata about this target
+
+			TargetInformation(std::string name, std::string regtype, std::string domain);
+			TargetInformation(std::string name, const int port);
+			TargetInformation(std::string name, const Dashel::Stream* dashelStream);
+
+			std::string dashel() const;
+		};
+
+		//! A Target allows client classes to choose and access Aseba targets.
+		class Target: public TargetInformation
+		{
+		public:
+			Target(std::string name, std::string regtype, std::string domain, Zeroconf & container);
+			Target(std::string name, const int port, Zeroconf & container);
+			Target(std::string name, const Dashel::Stream* dashelStream, Zeroconf & container);
+
+			~Target();
+
+			// disable copy constructor and copy assigment operator
+			Target(const Target &) = delete;
+			Target& operator=(const Target&) = delete;
+			// implement move versions instead
+			Target(Target && rhs);
+			Target& operator=(Target&& rhs);
+
+		protected:
+			// we do not want user code to use callbacks or play with the serviceRef
+			friend class Zeroconf; friend struct ZeroconfCallbacks;
+
+			void registerCompleted() const;
+			void updateCompleted() const;
+			void targetFound() const;
+
+			DNSServiceRef serviceRef{nullptr}; //!< Attached serviceRef
+
+			std::reference_wrapper<Zeroconf> container; //!< Back reference to containing Aseba::Zeroconf object
+		};
+
 		//! The list of targets being processed.
 		//! We do not care much of the O(n) performance when fetching a target as this vector is small and the lookup infrequent.
 		using Targets = std::vector<Target>;
+
 		//! Compute the hash of a TargetInformation object, using data from its parent TargetKey
 		struct TargetInformationKeyHash
 		{
@@ -83,6 +140,45 @@ namespace Aseba
 		};
 		//! Detected targets is a set of TargetInformation indexed by their TargetKey
 		using DetectedTargets = std::unordered_set<TargetInformation, TargetInformationKeyHash, TargetInformationKeyCompareEqual>;
+
+		/**
+			Zeroconf records three DNS records for each service: a PTR for the
+			symbolic name, an SRV for the actual host and port number, and a TXT
+			for extra information. The TXT record is used by clients to help choose
+			the hosts it wishes to connect to. The syntax of TXT records is defined
+			in https://tools.ietf.org/html/rfc6763#section-6.
+			The TXT record for Aseba targets specifies the target type, the Aseba
+			protocol version, a list of node ids, and a list of node product ids.
+		*/
+		class TxtRecord
+		{
+		public:
+			//! The fields are contained as a map of strings to strings, the constraints are enforced through the assign functions.
+			using Fields = std::map<std::string, std::string>;
+
+		public:
+			TxtRecord(const unsigned int protovers, const std::string& type, bool busy, const std::vector<unsigned int>& ids, const std::vector<unsigned int>& pids);
+			TxtRecord(const std::string & txtRecord);
+			TxtRecord(const unsigned char * txtRecord, uint16_t txtLen);
+
+			// Different kinds of keys can be encoded into TXT records
+			void assign(const std::string& key, const std::string& value);
+			void assign(const std::string& key, const int value);
+			void assign(const std::string& key, const std::vector<unsigned int>& values);
+			void assign(const std::string& key, const bool value);
+
+			const std::string at (const std::string& k) const { return fields.at(k); }
+			Fields::const_iterator begin() const { return fields.begin(); }
+			Fields::const_iterator end() const { return fields.end(); }
+
+			std::string record() const;
+
+		private:
+			void serializeField(std::ostringstream& txt, const std::string& key) const;
+
+		private:
+			Fields fields;
+		};
 
 	public:
 		// Default constructors and assignment operators
@@ -138,122 +234,7 @@ namespace Aseba
 		DNSServiceRef browseServiceRef{nullptr};
 	};
 
-	/**
-	 \addtogroup zeroconf
-		An error in registering or browsing Zeroconf
-	*/
-	struct Zeroconf::Error: public std::runtime_error
-	{
-		Error(const std::string & what): std::runtime_error(what) {}
-	};
-
-	/**
-	 \addtogroup zeroconf
-		A Zeroconf::TargetKey allows to uniquely identify a target on the network within Zeroconf.
-		It contains the informations returned by browse().
-	*/
-	struct Zeroconf::TargetKey
-	{
-		std::string name; //!< the full name of this target
-		std::string regtype{"_aseba._tcp"}; //!< the mDNS registration type
-		std::string domain{"local."}; //!< the domain of the host
-	};
-
 	bool operator ==(const Zeroconf::TargetKey &lhs, const Zeroconf::TargetKey &rhs);
-
-	/**
-	 \addtogroup zeroconf
-		A Zeroconf::TargetInformation allows client classes to choose and access Aseba targets.
-	*/
-	struct Zeroconf::TargetInformation: Zeroconf::TargetKey
-	{
-		// metadata about this target after resolution
-		std::string host; //!< the Dashel target connection ports
-		int port{0}; //!< the Dashel target connection port
-
-		std::map<std::string, std::string> properties; //!< User-modifiable metadata about this target
-
-		TargetInformation(std::string name, std::string regtype, std::string domain);
-		TargetInformation(std::string name, const int port);
-		TargetInformation(std::string name, const Dashel::Stream* dashelStream);
-
-		std::string dashel() const;
-	};
-
-	/**
-	 \addtogroup zeroconf
-
-		A Zeroconf::Target allows client classes to choose and access Aseba targets.
-	 */
-	class Zeroconf::Target: public Zeroconf::TargetInformation
-	{
-	public:
-		Target(std::string name, std::string regtype, std::string domain, Zeroconf & container);
-		Target(std::string name, const int port, Zeroconf & container);
-		Target(std::string name, const Dashel::Stream* dashelStream, Zeroconf & container);
-
-		~Target();
-
-		// disable copy constructor and copy assigment operator
-		Target(const Target &) = delete;
-		Target& operator=(const Target&) = delete;
-		// implement move versions instead
-		Target(Target && rhs);
-		Target& operator=(Target&& rhs);
-
-	protected:
-		// we do not want user code to use callbacks or play with the serviceRef
-		friend class Zeroconf; friend struct ZeroconfCallbacks;
-
-		void registerCompleted() const;
-		void updateCompleted() const;
-		void targetFound() const;
-
-		DNSServiceRef serviceRef{nullptr}; //!< Attached serviceRef
-
-		std::reference_wrapper<Zeroconf> container; //!< Back reference to containing Aseba::Zeroconf object
-	};
-
-	/**
-	 \addtogroup zeroconf
-
-		Zeroconf records three DNS records for each service: a PTR for the
-		symbolic name, an SRV for the actual host and port number, and a TXT
-		for extra information. The TXT record is used by clients to help choose
-		the hosts it wishes to connect to. The syntax of TXT records is defined
-		in https://tools.ietf.org/html/rfc6763#section-6.
-		The TXT record for Aseba targets specifies the target type, the Aseba
-		protocol version, a list of node ids, and a list of node product ids.
-	 */
-	class Zeroconf::TxtRecord
-	{
-	public:
-		//! The fields are contained as a map of strings to strings, the constraints are enforced through the assign functions.
-		using Fields = std::map<std::string, std::string>;
-		
-	public:
-		TxtRecord(const unsigned int protovers, const std::string& type, bool busy, const std::vector<unsigned int>& ids, const std::vector<unsigned int>& pids);
-		TxtRecord(const std::string & txtRecord);
-		TxtRecord(const unsigned char * txtRecord, uint16_t txtLen);
-
-		// Different kinds of keys can be encoded into TXT records
-		void assign(const std::string& key, const std::string& value);
-		void assign(const std::string& key, const int value);
-		void assign(const std::string& key, const std::vector<unsigned int>& values);
-		void assign(const std::string& key, const bool value);
-
-		const std::string at (const std::string& k) const { return fields.at(k); }
-		Fields::const_iterator begin() const { return fields.begin(); }
-		Fields::const_iterator end() const { return fields.end(); }
-
-		std::string record() const;
-
-	private:
-		void serializeField(std::ostringstream& txt, const std::string& key) const;
-
-	private:
-		Fields fields;
-	};
 
 	/*@}*/
 } // namespace Aseba
