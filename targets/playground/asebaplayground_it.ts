41049e164e5136e8d9dff119fb074b8b06cfec62
Michael Bonani <michael.bonani@mobsya.org>
2017-12-12 16:15:47 +0100

    italian traduction by Paolo Rossetti

--- targets/playground/asebaplayground_it.ts
+++ targets/playground/asebaplayground_it.ts
@@ -7,113 +7,113 @@
         <location filename="PlaygroundViewer.cpp" line="+82"/>
         <source>You are using a feature not available in the simulator, click here to buy a real Thymio.</source>
         <oldsource>You are using a feature not available in the simulator, click here to buy a real Thymio</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>Stai usando una funzione non disponibile nel simulatore, clicca qui per acquistare un vero Thymio.</translation>
     </message>
     <message>
         <location line="+0"/>
         <source>https://www.thymio.org/en:thymiobuy</source>
-        <translation type="unfinished"></translation>
+        <translation>https://www.thymio.org/it:thymiobuy</translation>
     </message>
     <message>
         <location line="+11"/>
         <location line="+8"/>
         <source>Target %0, cannot read from socket: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Destinazione %0, non è possibile leggere da socket: %1</translation>
     </message>
     <message>
         <location line="-4"/>
         <source>New client connected from %0</source>
-        <translation type="unfinished"></translation>
+        <translation>Nuovo client collegato da %0</translation>
     </message>
     <message>
         <location line="+8"/>
         <source>Client disconnected properly from %0</source>
-        <translation type="unfinished"></translation>
+        <translation>Client disconnesso correttamente da %0</translation>
     </message>
     <message>
         <location line="+4"/>
         <source>Client disconnected abnormally from %0</source>
-        <translation type="unfinished"></translation>
+        <translation>Client scollegato in modo improprio da %0</translation>
     </message>
     <message>
         <location line="+4"/>
         <source>Old client disconnected from %0</source>
-        <translation type="unfinished"></translation>
+        <translation>Client precedente disconnesso da %0</translation>
     </message>
     <message>
         <location line="+14"/>
         <source>Cannot create listening port %0: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Impossibile creare la porta di ascolto %0: %1</translation>
     </message>
     <message>
         <location line="+40"/>
         <source>%0: Process failed to start</source>
-        <translation type="unfinished"></translation>
+        <translation>%0: Impossibile avviare il processo</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>%0: Process crashed</source>
-        <translation type="unfinished"></translation>
+        <translation>%0: Processo terminato</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>%0: Write error</source>
-        <translation type="unfinished"></translation>
+        <translation>%0: Errore di scrittura</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>%0: Read error</source>
-        <translation type="unfinished"></translation>
+        <translation>%0: Errore di lettura</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>%0: Unknown error</source>
-        <translation type="unfinished"></translation>
+        <translation>%0: Errore sconosciuto</translation>
     </message>
     <message>
         <location line="+12"/>
         <source>%0: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>%0: %1</translation>
     </message>
     <message>
         <location line="+104"/>
         <source>Aseba Playground is a simulator for robots that can be programmed through Aseba.</source>
-        <translation type="unfinished"></translation>
+        <translation>Aseba Playground è un simulatore per robot programmabili tramite Aseba.</translation>
     </message>
     <message>
         <location line="+1"/>
         <source>https://www.thymio.org/en:thymiosimulation</source>
-        <translation type="unfinished"></translation>
+        <translation>https://www.thymio.org/it:thymiosimulation</translation>
     </message>
     <message>
         <location line="+1"/>
         <source>You can move the camera and objects/robots the following way:</source>
-        <translation type="unfinished"></translation>
+        <translation>È possibile spostare la videocamere e gli oggetti/robot nel modo seguente:</translation>
     </message>
     <message>
         <location line="+2"/>
         <source>Left click on an object: Select the object.</source>
-        <translation type="unfinished"></translation>
+        <translation>Clic sinistro del mouse su un oggetto: Seleziona l&apos; oggetto.</translation>
     </message>
     <message>
         <location line="+1"/>
         <source>Left click outside an object: De-select the object.</source>
-        <translation type="unfinished"></translation>
+        <translation>Clic sinistra delmouse all&apos;esterno di un oggetto: deseleziona l&apos; oggetto.</translation>
     </message>
     <message>
         <location line="+1"/>
         <source>Left drag: If an object is selected, move it, otherwise move the camera.</source>
-        <translation type="unfinished"></translation>
+        <translation>Trascina a sinistra: Se si seleziona un oggetto, spostarlo, altrimenti spostare la videocamera.</translation>
     </message>
     <message>
         <location line="+1"/>
         <source>Right drag: If an object is selected, rotate it, otherwise rotate the camera.</source>
-        <translation type="unfinished"></translation>
+        <translation>Trascina a destra: se si seleziona un oggetto, ruotarlo, altrimenti ruotare la videocamera.</translation>
     </message>
     <message>
         <location line="+1"/>
         <source>Mouse wheel or left drag + shift: Zoom camera.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ruota del mouse o trascinamento sinistro + Shift: Zoom della videocamera.</translation>
     </message>
 </context>
 <context>
@@ -121,7 +121,7 @@
     <message>
         <location filename="playground.cpp" line="+448"/>
         <source>%2 on %3</source>
-        <translation type="unfinished"></translation>
+        <translation>%2 su %3</translation>
     </message>
 </context>
 </TS>
