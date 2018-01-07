579949879b8104c846722261aeba4bc050414e13
Shiling Wang <shiling.wang@enlightware.ch>
2018-01-07 22:21:48 +0100

    Complete Chinese translation.

--- targets/challenge/asebachallenge_zh.ts
+++ targets/challenge/asebachallenge_zh.ts
@@ -1,42 +1,42 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh">
+<TS version="2.0" language="zh">
 <context>
     <name>Enki::ChallengeApplication</name>
     <message>
         <location filename="challenge.cpp" line="+889"/>
         <source>Aseba Challenge Help</source>
-        <translation type="unfinished"></translation>
+        <translation>Aseba挑战帮助</translation>
     </message>
     <message>
         <location line="+15"/>
         <source>Add a new robot</source>
-        <translation type="unfinished"></translation>
+        <translation>添加一个新的机器人</translation>
     </message>
     <message>
         <location line="+2"/>
         <source>Remove all robots</source>
-        <translation type="unfinished"></translation>
+        <translation>移除所有机器人</translation>
     </message>
     <message>
         <location line="+2"/>
         <source>Auto camera</source>
-        <translation type="unfinished"></translation>
+        <translation>摄像头模式</translation>
     </message>
     <message>
         <location line="+2"/>
         <source>Full screen</source>
-        <translation type="unfinished"></translation>
+        <translation>全屏</translation>
     </message>
     <message>
         <location line="+4"/>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>帮助</translation>
     </message>
     <message>
         <location line="+2"/>
         <source>Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>退出</translation>
     </message>
 </context>
 <context>
@@ -44,12 +44,12 @@
     <message>
         <location line="-319"/>
         <source>Add a new robot</source>
-        <translation type="unfinished"></translation>
+        <translation>添加一个新的机器人</translation>
     </message>
     <message>
         <location line="+0"/>
         <source>Robot name:</source>
-        <translation type="unfinished"></translation>
+        <translation>机器人名字:</translation>
     </message>
 </context>
 <context>
@@ -57,17 +57,17 @@
     <message>
         <location line="+486"/>
         <source>Please choose your language</source>
-        <translation type="unfinished"></translation>
+        <translation>请选择语言</translation>
     </message>
     <message>
         <location line="+24"/>
         <source>Ok</source>
-        <translation type="unfinished"></translation>
+        <translation>Ok</translation>
     </message>
     <message>
         <location line="+4"/>
         <source>Language selection</source>
-        <translation type="unfinished"></translation>
+        <translation>语言选项</translation>
     </message>
 </context>
 <context>
@@ -75,12 +75,12 @@
     <message>
         <location line="-890"/>
         <source>Aseba Challenge</source>
-        <translation type="unfinished"></translation>
+        <translation>Aseba挑战</translation>
     </message>
     <message>
         <location line="+0"/>
         <source>Cannot create listening port %0: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>创建监听端口%0: %1失败</translation>
     </message>
 </context>
 </TS>
--- targets/playground/asebaplayground_zh.ts
+++ targets/playground/asebaplayground_zh.ts
@@ -1,119 +1,119 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_CN">
+<TS version="2.0" language="zh_CN">
 <context>
     <name>Enki::PlaygroundViewer</name>
     <message>
         <location filename="PlaygroundViewer.cpp" line="+82"/>
         <source>You are using a feature not available in the simulator, click here to buy a real Thymio.</source>
         <oldsource>You are using a feature not available in the simulator, click here to buy a real Thymio</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>您所用的功能不能在模拟器上实现,点击这里购买Thymio机器人</translation>
     </message>
     <message>
         <location line="+0"/>
         <source>https://www.thymio.org/en:thymiobuy</source>
-        <translation type="unfinished"></translation>
+        <translation>https://www.thymio.org/en:thymiobuy</translation>
     </message>
     <message>
         <location line="+11"/>
         <location line="+8"/>
         <source>Target %0, cannot read from socket: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>端口号%1不能读取目标%0</translation>
     </message>
     <message>
         <location line="-4"/>
         <source>New client connected from %0</source>
-        <translation type="unfinished"></translation>
+        <translation>%0链接新用户</translation>
     </message>
     <message>
         <location line="+8"/>
         <source>Client disconnected properly from %0</source>
-        <translation type="unfinished"></translation>
+        <translation>用户成功从%0断开链接</translation>
     </message>
     <message>
         <location line="+4"/>
         <source>Client disconnected abnormally from %0</source>
-        <translation type="unfinished"></translation>
+        <translation>用户从%0断开链接异常</translation>
     </message>
     <message>
         <location line="+4"/>
         <source>Old client disconnected from %0</source>
-        <translation type="unfinished"></translation>
+        <translation>老用户从%0断开连接</translation>
     </message>
     <message>
         <location line="+14"/>
         <source>Cannot create listening port %0: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>创建监听端口%0:%1失败</translation>
     </message>
     <message>
         <location line="+40"/>
         <source>%0: Process failed to start</source>
-        <translation type="unfinished"></translation>
+        <translation>%0:程序启动失败</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>%0: Process crashed</source>
-        <translation type="unfinished"></translation>
+        <translation>%0:程序死机</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>%0: Write error</source>
-        <translation type="unfinished"></translation>
+        <translation>%0:写入错误</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>%0: Read error</source>
-        <translation type="unfinished"></translation>
+        <translation>%0:读取错误</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>%0: Unknown error</source>
-        <translation type="unfinished"></translation>
+        <translation>%0:未知错误</translation>
     </message>
     <message>
         <location line="+12"/>
         <source>%0: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>%0:%1</translation>
     </message>
     <message>
         <location line="+104"/>
         <source>Aseba Playground is a simulator for robots that can be programmed through Aseba.</source>
-        <translation type="unfinished"></translation>
+        <translation>Aseba游乐场是一款可用Aseba对机器人进行编程的仿真软件</translation>
     </message>
     <message>
         <location line="+1"/>
         <source>https://www.thymio.org/en:thymiosimulation</source>
-        <translation type="unfinished"></translation>
+        <translation>https://www.thymio.org/en:thymiosimulation</translation>
     </message>
     <message>
         <location line="+1"/>
         <source>You can move the camera and objects/robots the following way:</source>
-        <translation type="unfinished"></translation>
+        <translation>你可以通过以下方式移除摄像头和机器人:</translation>
     </message>
     <message>
         <location line="+2"/>
         <source>Left click on an object: Select the object.</source>
-        <translation type="unfinished"></translation>
+        <translation>鼠标左键按住物体:选择该物体</translation>
     </message>
     <message>
         <location line="+1"/>
         <source>Left click outside an object: De-select the object.</source>
-        <translation type="unfinished"></translation>
+        <translation>鼠标左键点击除物体外其他地方:取消选择该物体</translation>
     </message>
     <message>
         <location line="+1"/>
         <source>Left drag: If an object is selected, move it, otherwise move the camera.</source>
-        <translation type="unfinished"></translation>
+        <translation>鼠标左键拖拽:如果一个物体被选中,该物体被移动,否则摄像头被移动</translation>
     </message>
     <message>
         <location line="+1"/>
         <source>Right drag: If an object is selected, rotate it, otherwise rotate the camera.</source>
-        <translation type="unfinished"></translation>
+        <translation>鼠标右键拖拽:如果一个物体被选中,旋转该物体,否则旋转摄像头</translation>
     </message>
     <message>
         <location line="+1"/>
         <source>Mouse wheel or left drag + shift: Zoom camera.</source>
-        <translation type="unfinished"></translation>
+        <translation>鼠标滚轮或左键拖拽+shift:放大或缩小摄像头视角</translation>
     </message>
 </context>
 <context>
@@ -121,7 +121,7 @@
     <message>
         <location filename="playground.cpp" line="+448"/>
         <source>%2 on %3</source>
-        <translation type="unfinished"></translation>
+        <translation>%2在%3上</translation>
     </message>
 </context>
 </TS>
