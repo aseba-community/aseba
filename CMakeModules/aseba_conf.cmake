a0b3f831c244f2003a26541ede61c08b2372d868
Corentin Jabot <corentin.jabot@mobsya.org>
2018-01-22 18:24:13 +0100

    Modernize cmake files

---
+++ CMakeModules/aseba_conf.cmake
@@ -0,0 +1,9 @@
+add_library(aseba_conf INTERFACE)
+target_link_libraries(aseba_conf cpp_features)
+
+target_compile_definitions(aseba_conf INTERFACE -DASEBA_ASSERT)
+
+# reduce the amount of recursive include trash on Windows
+if (WIN32)
+	target_compile_definitions(aseba_conf INTERFACE -DWIN32_LEAN_AND_MEAN -DNOMINMAX)
+endif()
