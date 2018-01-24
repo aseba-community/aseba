5d18ac00445442f30bad0720980b8e4c918cceea
Corentin Jabot <corentin.jabot@mobsya.org>
2018-01-24 12:34:55 +0100

    Source tree reorganization.
    This clean up the layout of the project.
    
    Sources are moved to aseba/
    Some docs are move to docs/. This will be improved later.
    
    Compilation of examples and tests is intentionnally broken and
    will be fixed in a subsequent commit.
    
    Maintainers scripts have been modified to been callable
    from anywhere.

--- CMakeModules/ParseVersion.cmake
+++ CMakeModules/ParseVersion.cmake
@@ -41,7 +41,7 @@ if (GIT_FOUND AND HAS_GIT_REP)
 	# write a file with the GIT_REV define
 	file(WRITE ${CMAKE_BINARY_DIR}/version.h.txt "#define ASEBA_BUILD_VERSION \"git-${GIT_REV}\"\n")
 	# write NSIS version file (for Windows build)
-	file(READ "${PROJECT_SOURCE_DIR}/common/consts.h" COMMON_CONSTS_H)
+	file(READ "${PROJECT_SOURCE_DIR}/aseba/common/consts.h" COMMON_CONSTS_H)
 	string(REGEX MATCH [0-9]+\\.[0-9]+\\.[0-9]+ ASEBA_VERSION ${COMMON_CONSTS_H})
 	file(WRITE ${CMAKE_BINARY_DIR}/version.nsi "!define VERSION \"${ASEBA_VERSION}-git-${GIT_REV}\"\n")
 	# copy the file to the final header only if the version changes
